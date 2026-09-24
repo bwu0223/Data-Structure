ee895a088b17a7ec1d94e356a33d874988538599
bwu0223 <bwu4533@gmail.com>
2026-09-24 16:09:31 +0800

    modify

--- Data_Structure_Q3.cpp
+++ Data_Structure_Q3.cpp
@@ -8,7 +8,7 @@ int main() {
     for (int i = a; i <= b; i++){
         if (i < 2){ 
             continue;
-        } 
+        }
         bool prime = true;
 
         for (int j = 2; j * j <= i; j++) {
